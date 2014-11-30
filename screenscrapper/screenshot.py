d84677e9b440892fa409fdcbd3a03a69785181c8
trilopin <jmpeso@gmail.com>
2014-11-30 03:00:17 +0100

    better doc, pep8 and js

--- screenscrapper/screenshot.py
+++ screenscrapper/screenshot.py
@@ -1,3 +1,4 @@
+"""Claas screenshot"""
 import os
 from PIL import Image
 import redis
@@ -9,21 +10,30 @@ from screenscrapper.util import validate_url
 class ScreenShot(object):
     """Screenshot class"""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, **kwargs):
         if 'phantom' in kwargs.keys():
             self.phantom = kwargs['phantom']
 
         if 'redis' in kwargs.keys():
-            self.redis = kwargs['redis']
+            self.redis = redis.StrictRedis(host=kwargs['redis'])
 
-    def get(self,url,size):
-        red = redis.StrictRedis(host=self.redis)
-        return red.get(self._unique_key(url, size))
 
-    def take(self,url,size):
-        """Take screenshot"""
-        if not validate_url(url):
+    def get(self, url, size):
+        """Get image from database"""
+        return self.redis.get(self._unique_key(url, size))
+
+
+    def exists(self, url, size):
+        """Check if exists in redis storage"""
+        return self.redis.exists(self._unique_key(url, size))
+
+
+    def take(self, url, size):
+        """Take screenshot: get unique filename,
+        pass data to phantomjs and call redis storage"""
+        if not validate_url(url) or self.exists(url, size):
             return
+
         tmpname = self._unique_path(size)
         print url
         script = os.path.dirname(os.path.abspath(__file__)) + \
@@ -36,25 +46,26 @@ class ScreenShot(object):
         os.system(command)
         self._store_image(url, size, tmpname)
 
+
     def _unique_path(self, size):
-        """Path for temp file"""
+        """Unique tmpfile for temp file"""
         return '/app/{0}_{1}x{2}.png'.format(uuid.uuid4(),
                                              size['width'],
                                              size['height'])
 
+
     def _unique_key(self, url, size):
-        """Key for redis store"""
-        return '{1}x{2}__{0}'.format(url.replace('http://','').strip(),
+        """Unique Key for redis storage"""
+        return '{1}x{2}__{0}'.format(url.replace('http://', '').strip(),
                                      size['width'],
                                      size['height'])
 
     def _store_image(self, url, size, filename):
-        """Store image in redis"""
+        """Store image in redis: use StringIO and Pillow image management"""
         output = StringIO.StringIO()
         image = Image.open(filename)
         image.save(output, format=image.format, optimize=True, quality=80)
-        red = redis.StrictRedis(host=self.redis)
-        red.set(self._unique_key(url, size), output.getvalue())
+        self.redis.set(self._unique_key(url, size), output.getvalue())
         output.close()
         os.remove(filename)
 
